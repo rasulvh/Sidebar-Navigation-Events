63c5d3f73d09acbdaabe98180fa4369c71c4c2dd
rasulvh <rasulvh@code.edu.az>
2023-02-25 19:42:56 +0400

    task2 was added

---
+++ assets/js/task2.js
@@ -0,0 +1,37 @@
+// let open = document.querySelector(".open")
+// let close = document.querySelector(".close")
+// let right_sidebar = document.querySelector(".right-sidebar")
+
+// open.addEventListener("click", function() {
+//     right_sidebar.classList.remove("right-hide")
+//     this.classList.add("d-none")
+//     close.classList.remove("d-none")
+// })
+
+// close.addEventListener("click", function() {
+//     right_sidebar.classList.add("right-hide")
+//     this.classList.add("d-none")
+//     open.classList.remove("d-none")
+// })
+
+
+
+
+
+
+
+// let open = document.querySelector(".open")
+// let close = document.querySelector(".close")
+// let down_sidebar = document.querySelector(".down-sidebar")
+
+// open.addEventListener("click", function() {
+//     down_sidebar.classList.remove("down-hide")
+//     this.classList.add("d-none")
+//     close.classList.remove("d-none")
+// })
+
+// close.addEventListener("click", function() {
+//     down_sidebar.classList.add("down-hide")
+//     this.classList.add("d-none")
+//     open.classList.remove("d-none")
+// })
