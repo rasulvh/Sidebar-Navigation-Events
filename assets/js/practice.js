5105ab3865977f45014f35c6fb2083dedf047982
rasulvh <rasulvh@code.edu.az>
2023-02-25 17:28:49 +0400

    practice was added

---
+++ assets/js/practice.js
@@ -0,0 +1,72 @@
+// let open = document.querySelector(".open")
+// let close = document.querySelector(".close")
+// let sidebar = document.querySelector(".sidebar")
+
+// open.addEventListener("click", function() {
+//     sidebar.classList.remove("hide")
+//     this.classList.add("d-none")
+//     close.classList.remove("d-none")
+// })
+
+// close.addEventListener("click", function() {
+//     sidebar.classList.add("hide")
+//     this.classList.add("d-none")
+//     open.classList.remove("d-none")
+// })
+
+
+// let button = document.querySelector("button")
+
+// console.log(button.parentNode.parentNode);
+
+// console.log(button.previousElementSibling.innerHTML);
+
+// console.log(button.nextElementSibling);
+
+// let elems = button.parentNode.children
+
+// for (const item of elems) {
+//     if (item.innerText == "Hello") {
+//         item.style.color = "red"
+//     }
+// }
+
+// console.log(button.closest(".heads"));
+
+// let elem1 = document.querySelector(".gultac")
+// console.log(elem1.closest(".head").lastElementChild.innerHTML);
+
+// let elem2 = document.querySelector(".anar")
+// console.log(elem2.closest(".head").previousElementSibling.firstElementChild.firstElementChild.firstElementChild.innerHTML);
+
+// let elem3 = document.querySelector(".novreste")
+// console.log(elem3.closest(".container").previousElementSibling.children[1].innerHTML);
+
+let input = document.querySelector("input")
+
+// function inputEvent() {
+//     console.log("Input event");
+// }
+
+// input.addEventListener("change", inputEvent)
+// input.addEventListener("blur", inputEvent)
+// input.addEventListener("keyup", inputEvent)
+// input.addEventListener("keydown", inputEvent)
+
+// document.getElementById("test").addEventListener("change", function(){
+//     console.log(e.target.value);
+
+//     if(this.value == 1){
+//         input.value = "HTML"
+//     }
+// })
+
+// input.addEventListener("keyup", function(e){
+//     if (e.keyCode == 13) {
+//         document.querySelector("button").click()
+//     }
+// })
+
+// document.querySelector("button").addEventListener("click", function(e){
+//     alert(e.target.previousElementSibling.value)
+// })
